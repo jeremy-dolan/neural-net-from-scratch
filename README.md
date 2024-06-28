a793dd5dc1feb4b9a5b61fb38ac278f5a46dc640
Jeremy Dolan <jeremy.dolan@nyu.edu>
2024-06-28 12:24:16 -0400

    xor: major discussion edits, modularize final plot

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 Neural network framework, with some demo applications and analysis.
 
-* [neural_net_vanilla.py](neural_net_vanilla.py) - Complete feedforward neural network
-  framework, written in vanilla Python for educational purposes. It is wildly
-  inefficient. 🙃
+* [neural_net_vanilla.py](neural_net_vanilla.py) - Complete feedforward neural
+  network framework, written in vanilla Python for educational purposes. It is
+  <!-- well commented and --> wildly inefficient. 🙃
 
-* [xor-and-linear-separability.ipynb](xor-and-linear-separability.ipynb) -
-  This notebook discusses the infamous XOR function and linear
-  separability. Using our framework, we construct a multi-layer perceptron that
-  learns XOR. We then analyze the trained network and visualize its decision
-  boundaries.
+<!--
+* [neural_net_numpy.py](neural_net_numpy.py) - Complete feedforward neural network
+  framework, written with NumPy. It is mildly inefficient.
+-->
+
+* [xor-and-linear-separability.ipynb](xor-and-linear-separability.ipynb) - This
+  notebook discusses the XOR function and linear separability. We construct a
+  multi-layer perceptron that learns XOR, and then analyze the trained network
+  and graph its decision boundaries.
 
 <!--
 * [minst-net](3layer-minst-net.ipynb) - This notebook builds a digit classifier
--- plots/xor.py
+++ plots/xor.py
@@ -6,8 +6,6 @@ try:
 except ImportError:
     print("Unable to import matplotlib. Cannot generate figures but neural net code itself can run")
 
-plt.style.use("fivethirtyeight")
-
 True_XOR = [(0,1), (1,0)]
 False_XOR = [(0,0), (1,1)]
 True_OR = [(0,1), (1,0), (1,1)]
@@ -20,7 +18,6 @@ def setup_bool(ax:plt.Axes, true_points, false_points, label_points=True):
     # Draw and label Boolean input space
     ax.scatter(*zip(*true_points), c='blue', label='True', s=100)
     ax.scatter(*zip(*false_points), c='red', label='False', s=100)
-
     if label_points:
         for (x, y) in true_points + false_points:
             ax.text(x+0.04, y-0.1, f'({x}, {y})', fontsize=7)
@@ -36,6 +33,7 @@ def setup_bool(ax:plt.Axes, true_points, false_points, label_points=True):
 
 def plot_binary_Boolean_input_space_with_XOR_output():
     '''plot binary Boolean function input space, with colors indicating the output of XOR'''
+    plt.style.use("fivethirtyeight")
     fig = plt.figure(figsize=(3.5,2.5))
     ax = fig.add_subplot()
     setup_bool(ax, True_XOR, False_XOR, label_points=True)
@@ -70,7 +68,12 @@ def draw_boundary(ax:plt.Axes, y_int, slope,
 
 
 def plot_linear_separation_OR_vs_XOR():
+    plt.style.use("fivethirtyeight")
+
+    ax1: plt.Axes
+    ax2: plt.Axes
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (6.5, 3))
+    
     fig.subplots_adjust(wspace=0.5)
 
     setup_bool(ax1, True_OR, False_OR)
@@ -97,6 +100,9 @@ def plot_functions(sigmoid, sigmoid_derivative, log_loss):
     plt.style.use("default")
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 3))
+    ax1:plt.Axes
+    ax2:plt.Axes
+
     fig.subplots_adjust(wspace=0.3)
 
     x1 = [-5 + 0.25*i for i in range(40)]
@@ -120,5 +126,100 @@ def plot_functions(sigmoid, sigmoid_derivative, log_loss):
     for ax in (ax1, ax2):
         ax.grid(True)
         ax.legend(fontsize=9)
+
+    plt.show()
+
+
+def setup_bool_gradient(ax:plt.Axes, meshgrid, title=None):
+    ax.set(xticks=[0, 1], yticks=[0, 1], xlim=(-0.5, 1.5), ylim=(-0.5, 1.5))
+    ax.set_aspect('equal') # make sure it's square
+    ax.set_title(title, fontsize=12)
+
+    # Draw and label XOR input/outputs
+    ax.scatter(*zip(*True_XOR), color=(0,0,0), label='True', s=100, marker='.')
+    ax.scatter(*zip(*False_XOR), color=(0,0,0), label='False', s=80, marker='x')
+    for (x, y) in True_XOR + False_XOR:
+        ax.text(x+0.02, y-0.2, f'({x}, {y})', fontsize=8)
+
+    # Draw the x and y axes
+    # ax.axhline(0, color='white', linewidth=0.5, linestyle='-.')
+    # ax.axvline(0, color='white', linewidth=0.5, linestyle='-.')
+    # ax.axhline(0, color='black', linewidth=1, linestyle='dotted')
+    # ax.axvline(0, color='black', linewidth=1, linestyle='dotted')
+
+    # Overlay our activation values
+    gradient = ax.imshow(
+        meshgrid,
+        extent=[-0.5, 1.5, -0.5, 1.5],
+        origin='lower',     # data is x,y coordinates, not i,j row-columns
+        cmap='coolwarm_r',  # red (0) to blue (1) gradient
+        alpha=0.7           # make it lighter
+    )
+    return gradient
+
+
+def plot_meshgrid(meshgrid, title):
+    ax:plt.Axes
+    fig, ax = plt.subplots(figsize=(4,3))
+
+    color_grad = setup_bool_gradient(ax, meshgrid, title)
+    cbar = plt.colorbar(color_grad)    # label="activation $a = \sigma(z)$")
+    cbar.set_ticks([0, 0.25, 0.5, 0.75, 1])
+
+    # plt.legend()
+    plt.show()
+
+
+def plot_net_meshgrids(node1grid, node2grid, outputgrid):
+    plt.style.use('default')
+
+    fig = plt.figure(figsize=(8, 6))
+    gs = gridspec.GridSpec(4, 2, wspace=0.3, hspace=0.4)
+    # (wspace = width between columns as a fraction of the average axis width)
+    #gs = gridspec.GridSpec(1, 4, width_ratios=[1, 1, 1, 0.05])
+    ax1 = fig.add_subplot(gs[0:2, 0])      # top-left plot
+    ax2 = fig.add_subplot(gs[2:4, 0])      # bottom-left plot
+    ax3 = fig.add_subplot(gs[1:3, 1])      # center-right plot
+
+    ax1.set_ylabel('Node 1', fontsize=12) #, rotation=0)
+    ax2.set_ylabel('Node 2', fontsize=12)
+
+    setup_bool_gradient(ax1, node1grid, 'Layer 1')
+    setup_bool_gradient(ax2, node2grid)
+    color_grad = setup_bool_gradient(ax3, outputgrid, 'Output Node')
+
+    # Create a single colorbar for all subplots
+    cax = fig.add_subplot(gs[3, 1])
+    cbar = plt.colorbar(color_grad, cax=cax, orientation='horizontal', ticks=[0, 1], label='classification')
+    cbar.ax.xaxis.set_label_position('top')
+    cbar.ax.set_position([0.61, 0.18, 0.25, 0.025])  # [left, bottom, width, height] in figure coordinates
+    cbar.set_ticks([0, 1])
+    cbar.set_ticklabels(['False', 'True'])
+
+    # create a single legend for all subplots
+    handles, labels = ax3.get_legend_handles_labels()
+    fig.legend(handles, labels, ncol=2, fontsize=8, title='XOR target classifications', title_fontsize=9,
+            loc='lower right', bbox_to_anchor=(0.85, 0.06), bbox_transform=fig.transFigure)
+
+    def middle_of_right_spline(ax:plt.Axes):
+        bbox = ax.get_position()
+        return [bbox.x1,       (bbox.y0 + bbox.y1)/2]
+        return [bbox.x1-0.001, (bbox.y0 + bbox.y1)/2] # nudge x value due to line curvature
+    def middle_of_left_spline(ax:plt.Axes):
+        bbox = ax.get_position()
+        return [bbox.x0, (bbox.y0 + bbox.y1)/2]
+
+    arrow_1_start = middle_of_right_spline(ax1)
+    arrow_1_stop = middle_of_left_spline(ax3)
+    arrow_2_start = middle_of_right_spline(ax2)
+    arrow_2_stop = middle_of_left_spline(ax3)
+
+    arrow1 = FancyArrowPatch(arrow_1_start, arrow_1_stop, linewidth=1.25, color='black', arrowstyle='->', 
+                            connectionstyle="arc,angleA=5,angleB=180,armA=50,armB=75,rad=50", mutation_scale=20)
+    arrow2 = FancyArrowPatch(arrow_2_start, arrow_2_stop, linewidth=1.25, color='black', arrowstyle='->', 
+                            connectionstyle="arc,angleA=-5,angleB=180,armA=50,armB=75,rad=50", mutation_scale=20)
+
+    fig.add_artist(arrow1)
+    fig.add_artist(arrow2)
 
     plt.show()
